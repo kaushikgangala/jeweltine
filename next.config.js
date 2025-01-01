a7bc6019d24e5856de854510500e67e3f63ca8ef
kaushikgangala <kaushikgangala@gmail.com>
2025-01-01 16:42:19 +0530

    removed future and webpack parameters from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    console.log(options.webpack.version); // Should be webpack v5 now
-    config.experiments = {};
-    return config;
-  },
   reactStrictMode: true,
   images: {
     domains: [
