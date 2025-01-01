501a57dbff0e3f9812b82f080dbddf068fb9b5fd
kaushikgangala <kaushikgangala@gmail.com>
2025-01-01 16:36:45 +0530

    deleted -kaushik files

---
+++ app/contact/page-kaushik.js
@@ -0,0 +1,29 @@
+// app/contact/page.js
+
+import Footer from "../../components/Footer";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow max-w-4xl mx-auto p-8">
+          <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">
+            Contact Us
+          </h1>
+          <p className="text-lg text-gray-700 text-center">
+            You can reach us via email at{" "}
+            <a
+              href="mailto:contact.jeweltine@gmail.com"
+              className="text-blue-500 hover:underline"
+            >
+              contact.jeweltine@gmail.com
+            </a>
+          </p>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Contact;
