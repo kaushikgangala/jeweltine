6a7d7f48d4be8fa55d6178115d6cc0655416493b
kaushikgangala <kaushikgangala@gmail.com>
2025-01-01 19:24:03 +0530

    change images order excluding the customizable necklace

--- constants/products.js
+++ constants/products.js
@@ -1,10 +1,10 @@
 export const products = {
   chess: {
     images: [
+      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-eedbc586-2b46-411b-97e7-8c4e596b2c30.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-13965-55e69d2b-1219-495b-8b2d-421b711a8cc8.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1729-a956aca9-af05-4b11-baed-6189dba54750.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9365-d055753f-7ec8-4f60-bfed-79ffa0b12077.jpeg",
-      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-eedbc586-2b46-411b-97e7-8c4e596b2c30.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9368-6fb99460-1133-4a9e-8b17-8c8b0685a013.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9369-5cff8b0e-4c61-491e-960c-eb1cb403aef7.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1728-75c9c48a-f62a-48c5-945a-46dbad8b8c0a.jpeg",
@@ -18,10 +18,10 @@ export const products = {
   },
   programming: {
     images: [
+      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-a6930080-2c6e-498a-a77d-2dd816b75312.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-13965-efbb684b-f334-49b4-99a2-8d4eabb5f595.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1729-7347595d-8ae1-4287-9679-3c9eb71ab1d7.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9365-a0a1b6ec-5083-4b5b-9f60-28aaef8c3ad0.jpeg",
-      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-a6930080-2c6e-498a-a77d-2dd816b75312.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9368-4d0b078b-437e-4a67-90ad-1e4df90eedc6.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9369-32523cb2-29e5-42bd-bce1-afcb23dfdd36.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1728-ab97d71e-26b8-45d3-ac64-7ea4fdebd418.jpeg",
@@ -35,10 +35,10 @@ export const products = {
   },
   fitness: {
     images: [
+      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-b676ec14-3619-42fe-817e-182309e5ede7.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-13965-919cacf6-8202-4357-b8c9-c19ed38ec51a.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1729-a685d216-3eaf-49d7-9311-806573a5e8f5.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9365-6d890062-c13d-491d-ad6e-f0754f8247f3.jpeg",
-      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-b676ec14-3619-42fe-817e-182309e5ede7.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9368-a8a97f09-29b7-4c94-b663-7d84025187f4.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9369-88ccf88a-8f92-4d10-bf8e-d4438a16ecb2.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1728-8a98f9af-da27-45ea-a2b6-b5a032ca88e1.jpeg",
@@ -52,10 +52,10 @@ export const products = {
   },
   coffee: {
     images: [
+      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-49c23f3d-59bb-4717-bb7d-32fe4dd962bb.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-13965-29b97390-dca1-418d-96d8-0fef0becacc2.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1729-4ccb7584-bef0-4d22-ba51-71f7f97d1826.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9365-d42d9b5d-facf-4813-96ab-82be7471f0aa.jpeg",
-      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-49c23f3d-59bb-4717-bb7d-32fe4dd962bb.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9368-59f4fa1d-6594-43a7-89b0-d8de0b8ee51e.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9369-9b302548-0197-4b6e-bb2d-41e3f787f8b1.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1728-a68fddbd-4afa-4c75-98a2-c9606bbe2409.jpeg",
@@ -69,10 +69,10 @@ export const products = {
   },
   outdoors: {
     images: [
+      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-ced24238-e25a-45b7-9251-a9f714ffc3d6.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-13965-a18be831-3233-41ac-8729-e6459fe8313f.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1729-caeba69b-f418-4ed2-9fe6-b1b28b095b4e.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9365-f6bacc31-de76-46f6-bfd3-b4c9e5e8d672.jpeg",
-      "https://prod-rendering-engine.s3.amazonaws.com/transformation-9367-ced24238-e25a-45b7-9251-a9f714ffc3d6.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9368-05c8945e-574b-481c-b072-52335726a700.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-9369-9705c0ec-5faf-4c2e-b48f-02b5d5b80eb0.jpeg",
       "https://prod-rendering-engine.s3.amazonaws.com/transformation-1728-87024ef0-c7e4-4ce9-a1ac-275242917cb2.jpeg",
